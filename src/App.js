1f80f4c05e1d82dfb7d357f434ba509d1be07cf5
F1lya17 <f1limonoff.nick@yandex.ru>
2023-07-05 14:19:49 +0500

    Revert "fix make it with react"
    This reverts commit 50578b8f2f5d68b539ebc283c26db42a270f4ea7.

---
+++ src/App.js
@@ -0,0 +1,9 @@
+function App() {
+    return (
+        <div className="App">
+            Thats fck works
+        </div>
+    );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './reset.css';
+import './styles.css';
+import Header from "./components/Header"
+import Main from "./components/Main"
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <>
+        <Header />
+        <Main />
+    </>
+);
+
