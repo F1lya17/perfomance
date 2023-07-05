f2c26bcba2f13e9a5d68401428f727318c53b57c
F1lya17 <f1limonoff.nick@yandex.ru>
2023-07-05 16:31:45 +0500

    .

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import './styles.css';
 import Header from "./components/Header"
 import Main from "./components/Main"
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('app'));
 root.render(
     <>
         <Header />
